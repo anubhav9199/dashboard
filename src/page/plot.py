0f4add695508bf73628ba9fcb97e24f0de075fc8
Anubhav Sharma <anubhavsharma9199@gmail.com>
2021-01-26 18:05:00 +0530

    fixing plot page file

--- src/page/plot.py
+++ src/page/plot.py
@@ -1,7 +1,7 @@
 from Lib.utils import title_awesome
-from core.file_upload import FileUploadEngine
+from core.plots_n_graphs import PlotsAndGraphsEngine
 
 def write():
     title_awesome('Plots & Graphs')
-    FileUploadEngine.file_upload_main(
+    PlotsAndGraphsEngine.plots_n_graphs_main(
         'Plots & Graphs', 'Plotting data you gave.')
