8a32af434496299619c8a9a9dc354f5d6e209aa9
Anubhav Sharma <anubhavsharma9199@gmail.com>
2021-01-26 11:03:05 +0530

    fixing utils

--- Lib/utils.py
+++ Lib/utils.py
@@ -1,2 +1,13 @@
+import streamlit as st
+
 def render_page(menupage):
     menupage.write()
+
+
+def title_awesome(body: str):
+    st.title(
+        f"{body} "
+        # "[![Quantum](https://cdn.rawgit.com/sindresorhus/awesome/"
+        # "d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg)]"
+        # "(https://github.com/Avkash/mldl)"
+    )
